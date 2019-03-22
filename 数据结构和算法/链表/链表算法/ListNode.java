484b91a3c54057097291c1072a4b2911c4a0cff0
abner1977 <44076816+abner1977@users.noreply.github.com>
2019-03-22 17:41:49 +0800

    20190322
    定义链表节点对象

---
+++ 数据结构和算法/链表/链表算法/ListNode.java
@@ -0,0 +1,18 @@
+package com.abner.study;
+
+/**
+ * @Description:  节点类
+ * @auther: wangyizheng
+ * @date: 2019/3/21 16:59
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next=null;
+
+    ListNode(int val){
+        this.val=val;
+    }
+
+}
